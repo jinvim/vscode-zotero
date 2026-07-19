a55595fe60a16f2d65cbdd77ad0f5533cdae8791
Seonjin Lee <sjbase@me.com>
2026-07-19 16:49:56 +0900

    chore: change the naming of the commdand from openItemInZotero to openItemZotero

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,7 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(
         vscode.commands.registerCommand('zotero.searchLibrary', searchLibrary),
         vscode.commands.registerCommand('zotero.openItem', quickOpenItem),
-        vscode.commands.registerCommand('zotero.openItemInZotero', () => openItem('zotero')),
+        vscode.commands.registerCommand('zotero.openItemZotero', () => openItem('zotero')),
         vscode.commands.registerCommand('zotero.openItemPdf', () => openItem('pdf')),
         vscode.commands.registerCommand('zotero.openItemDoi', () => openItem('doi')),
         vscode.commands.registerCommand('zotero.tidyBib', tidyBib),
